0f138f02d7f723cf5fd4580b8be8f2eca72c986b
Suelen Santos <suelen_santos2002@outlook.com>
2025-01-05 03:37:36 -0300

    feature | todo initial taks list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
+import { useState } from 'react';
+
+import './App.css';
+
+import Todo from './components/todo';
+import TodoForm from './components/todo-form';
+
+
 function App() {
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      text: "Finish yesterday's task about js",
+      category: "Work",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Go to gym",
+      category: "Personal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Start my react studies",
+      category: "Study",
+      isCompleted: true,
+    },
+  ]);
 
   return (
-    <>
-      <div></div>
-    </>
-  )
+    <div className="app">
+      <h1 className="title">Task List</h1>
+      {/* TAKS LIST */}
+      <div className="todo-list">
+        {todo.map((todo) => (
+          < Todo 
+            key={todo.id} 
+            todo={todo} 
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App
